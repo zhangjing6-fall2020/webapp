7861d37e091b8f2d243e02de1e46c4bb42ca0be2
bh7cw <ibh7cw@gmail.com>
2020-11-03 11:00:38 -0500

    add applicationstop

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 set -v
 
-ls /var/lib/webapps/webapp
-ls -l /var/lib/webapps
-
 /var/lib/webapps/webapp > /dev/null 2> /dev/null < /dev/null &
-jobs
-
-ls /var/lib/webapps/webapp
-ls -l /var/lib/webapps
-
-sleep 10
-ls /var/lib/webapps/webapp
-ls -l /var/lib/webapps
-jobs
-jobs -l
---
+++ codedeploy/ApplicationStop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -v
+
+killall webapp
+exit 0
