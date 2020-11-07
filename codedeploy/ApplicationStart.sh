ab70b3bebbdff7b2486f4e8b287a1166813a1448
bh7cw <ibh7cw@gmail.com>
2020-11-07 16:28:59 -0500

    add cloud watch config file

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -4,7 +4,7 @@ set -v
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
+    -c file:/opt/amazon-cloudwatch-agent-schema.json \
     -s
 
 /var/lib/webapps/webapp > /dev/null 2> /dev/null < /dev/null &
