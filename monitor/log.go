242c08d293be8695d9bf319b3a1488310e452a76
bh7cw <ibh7cw@gmail.com>
2020-11-08 10:44:15 -0500

    change log gile to 0777

--- monitor/log.go
+++ monitor/log.go
@@ -8,7 +8,7 @@ import (
 func SetUpLog(){
 	//set up logrus
 	filename := "/var/log/webapp.log"
-	f, err := os.OpenFile(filename, os.O_WRONLY | os.O_CREATE, 0755)
+	f, err := os.OpenFile(filename, os.O_WRONLY | os.O_CREATE, 0777)
 	if err != nil {
 		log.Error(err)
 	}
