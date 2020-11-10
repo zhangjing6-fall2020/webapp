e77122e7c9e53ee921a595c074395c68aa1b5e3a
bh7cw <ibh7cw@gmail.com>
2020-11-09 19:42:28 -0500

    run webapp using sudo

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -7,4 +7,4 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -c file:/opt/amazon-cloudwatch-agent-schema.json \
     -s
 
-/var/lib/webapps/webapp > /dev/null 2> /dev/null < /dev/null &
+sudo /var/lib/webapps/webapp > /dev/null 2> /dev/null < /dev/null &
