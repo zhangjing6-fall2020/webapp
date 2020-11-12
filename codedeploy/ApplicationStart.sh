5f68095ab3f978edf52be75f7faafcdeae0de191
bh7cw <ibh7cw@gmail.com>
2020-11-11 19:01:23 -0500

    change to cloudwatch-config.json

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -4,7 +4,7 @@ set -v
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
-    -c file:/opt/amazon-cloudwatch-agent-schema.json \
+    -c file:/opt/cloudwatch-config.json \
     -s
 
 sleep 10
